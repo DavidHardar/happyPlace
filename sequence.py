cbecbb5788b2735725648d2ad4e8ed01aeb8fcf5
Davíð Örn Harðarson <davidh20@ru.is>
2020-09-03 11:26:06 +0000

    Loka loka niðurstaða

--- sequence.py
+++ sequence.py
@@ -2,16 +2,19 @@
 # Output the sequence.
 # 1. Accept input
 # 2. Use input to determine for how long the for loop will last.
-# 3. take the sum of the last three numbers in the sequence.
-# 4. Print the sequence.
+# 3. Establish starting variables.
+# 4. Take the sum of the last three numbers in the sequence add them together.
+# 5. Print the sum.
+# 6. Start the for loop again.
+# 7. Repeat for n-2 times. 
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-a = 3
-b = 2
-c = 1
-d = 0
-
-for i in range(n):
+num_for_refernce = 3
+num_1 = 2
+num_2 = 1
+num_3 = 0
+print(1,2, end=' ')
+for i in range(n-2):
     
     print_gildi = b+c+d
     a = print_gildi
